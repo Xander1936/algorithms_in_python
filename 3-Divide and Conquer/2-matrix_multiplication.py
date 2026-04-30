981fe7d91e5375624a2e5e44585a69ce46943271
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-04-30 10:27:58 +0100

    Lesson 3: Divide and Conquer - matrix_multiplication - native method

---
+++ 3-Divide and Conquer/2-matrix_multiplication.py
@@ -0,0 +1,25 @@
+# <>
+# Native Method: Multiplying two matrices using nested loops
+
+# 2X2 matrix "X"
+X = [[1, 2], 
+     [2, 3]]
+
+# 2X2 matrix "Y"
+Y = [[2, 3],
+     [3, 4]]
+
+# 2X2 matrix of "0", which added to our answer, gives the answer
+result = [[0, 0],
+          [0, 0]]
+
+# iterate through rows of X
+for i in range(len(X)):
+    # iterate through columns of Y
+    for j in range(len(Y[0])):
+        # iterate through rows of Y
+        for k in range(len(Y)):
+            result[i][j] += X[i][k] * Y[k][j]
+            
+for end in result:
+    print(end) 
